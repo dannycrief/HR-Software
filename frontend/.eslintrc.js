36a7a1f5ea76fa3576937c20d12b76c2ce0ce330
Stepan Kozurak <step.kozbvb@gmail.com>
2020-09-30 22:41:29 +0200

    Added eslint settings; modified models; made new migrations; updated packages and vue.config.js

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:vue/base'
+  ],
+  parserOptions: {
+    "parser": "babel-eslint",
+    "ecmaVersion": 2017,
+    "sourceType": "module"
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {}
+}
