d1ffe0b60aee257b0aafb315642841fffe250e19
Stepan Kozurak <step.kozbvb@gmail.com>
2020-10-10 14:31:44 +0200

    Created new deps, added new images

--- frontend/src/icons.js
+++ frontend/src/icons.js
@@ -2,16 +2,16 @@ import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faCheck } from '@fortawesome/free-solid-svg-icons';
 import {
   faGithub, faDocker,
   faPython, faVuejs,
   faNodeJs, faFontAwesomeFlag,
   faFacebookSquare, faTwitterSquare,
-  faYoutube,
+  faYoutube, faGoogle, faFacebook, faTelegramPlane
 } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faCheckCircle, faAngleRight, faGithub,
+library.add(faCheckCircle, faCheck, faAngleRight, faGithub,
   faDocker, faPython, faVuejs, faNodeJs, faFontAwesomeFlag,
-  faFacebookSquare, faTwitterSquare, faYoutube);
+  faFacebookSquare, faTwitterSquare, faYoutube, faGoogle, faFacebook,faTelegramPlane);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
